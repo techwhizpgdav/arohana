59dbd4ff7357a41b7a975c591e2155faf87f03b5
Nishant <ntih5565@gmail.com>
2024-02-19 18:36:17 +0530

    url changed

--- src/Functions/Constants.js
+++ src/Functions/Constants.js
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 export let isLogin = false;
 
 // export const API_URL = 'http://localhost:5000';
-export const API_URL = 'https://rishi.pgdavhyperion.in';
+export const API_URL = 'https://backend.pgdavhyperion.in';
 
 export const MobilePara = "Ārohaṇa is a highly anticipated three-day cultural festival at P.G.D.A.V. College, University of Delhi. Organized by students with faculty guidance, it celebrates the college's cultural diversity. The festival showcases talents in music, dance, drama, literature, and fine arts, blending traditional and contemporary elements. It features competitions, exhibitions, food stalls, and games, fostering community and new relationships among students. Ārohaṇa embodies the spirit of youth and cultural diversity, uniting students in a celebration of arts and culture."
 
