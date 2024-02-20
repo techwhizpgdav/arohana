cd949eb63987b96eaadb2924396128f91b5b6250
mr-robot369 <anubhavshukla870@gmail.com>
2024-02-21 00:01:43 +0530

    incharge cards added

--- src/Components/Teams/Teams.jsx
+++ src/Components/Teams/Teams.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import teamData from './teamData.json';
 import TeamCard from './TeamCard';
 import './Team.css';
+import InchargeCard from './InchargeCard';
 
 const Teams = () => {
     const [activeTab, setActiveTab] = useState('core_team');
@@ -18,50 +19,34 @@ const Teams = () => {
     return (
         <div className="w-full flex flex-col items-center team-wrapper">
             <div className="py-5">
-                <h1 className="text-5xl lg:text-6xl xl:text-7xl font-bold mt-4 mb-8 text-white underline font-serif" data-aos="fade-up">Team</h1>
+                <h1 className="main-head text-5xl lg:text-6xl xl:text-7xl font-bold mt-4 mb-8 font-serif">TEAM</h1>
             </div>
 
-            {/* Principal Card */}
             <div className="flex flex-col items-center mb-8">
-                <h2 className="text-3xl font-bold">Principal</h2>
-                <div className="mx-auto overflow-hidden rounded-full shadow-lg border-4 border-orange-600" style={{ width: '60%' }}>
-                    <img
-                        src="https://www.pgdavcollege.in/assets/front/images/principaldesk.jpg"
-                        className="object-cover transition-transform rounded-full hover:scale-105"
-                        style={{ width: '100%', height: '100%', clipPath: 'circle(50%)' }} />
-                </div>
-                <p className="mt-4 text-xl font-bold text-center">Prof. Krishna Sharma</p>
+                <h2 className="text-4xl font-extrabold mb-4 text-center text-yellow-500">PRINCIPAL</h2>
+                <InchargeCard data={teamData.principal[0]} />
             </div>
+
 
             {/* Faculty Mentors */}
-            <div className="flex flex-col items-center mb-8">
-                <h2 className="text-3xl font-bold">Faculty Mentors</h2>
-                <div className="flex justify-between gap-8">
-                    <div className="mx-auto overflow-hidden rounded-full shadow-lg border-4 border-orange-600" style={{ width: '40%' }}>
-                        <img
-                            src="https://www.pgdavcollege.in/assets/front/images/principaldesk.jpg"
-                            className="object-cover transition-transform rounded-full hover:scale-105"
-                            style={{ width: '100%', height: '100%', clipPath: 'circle(50%)' }} />
-                        <p className="mt-4 text-xl font-bold text-center">Prof. Krishna Sharma</p>
-                    </div>
-                    <div className="mx-auto overflow-hidden rounded-full shadow-lg border-4 border-orange-600" style={{ width: '40%' }}>
-                        <img
-                            src="https://www.pgdavcollege.in/assets/front/images/principaldesk.jpg"
-                            className="object-cover transition-transform rounded-full hover:scale-105"
-                            style={{ width: '100%', height: '100%', clipPath: 'circle(50%)' }} />
-                        <p className="mt-4 text-xl font-bold text-center">Prof. Krishna Sharma</p>
-                    </div>
+            <div className="w-full flex flex-col items-center mb-12">
+                <h2 className="text-4xl font-extrabold mb-4 text-center text-yellow-500">Faculty Mentors</h2>
+                <div className="w-1/2 flex flex-wrap justify-around gap-8">
+                    {teamData.incharge.map((member, index) => (
+                        <InchargeCard key={index} data={member} />
+                    ))}
                 </div>
             </div>
 
             {/* Tab Buttons */}
-            <div className="flex justify-center mb-4">
-                <button className={`mr-4 px-4 py-2 rounded-full ${activeTab === 'core_team' ? 'bg-orange-600 text-white' : 'bg-gray-300 '}`} onClick={() => handleTabClick('core_team')}>Core Team</button>
-                <button className={`px-4 py-2 rounded-full ${activeTab === 'web_developer' ? 'bg-orange-600 text-white' : 'bg-gray-300 '}`} onClick={() => handleTabClick('web_developer')}>Web Development Team</button>
+            <div className="flex justify-center">
+                <button className={`mr-4 px-6 py-3 font-bold rounded-lg shadow-lg whitespace-normal w-45 ${activeTab === 'core_team' ? 'bg-yellow-500 text-white hover:scale-105' : 'bg-gray-300 text-gray-700 hover:bg-gray-400'}`} onClick={() => handleTabClick('core_team')}>Core Team</button>
+                <button className={`px-6 py-3 font-bold rounded-lg shadow-lg whitespace-normal w-45 ${activeTab === 'web_developer' ? 'bg-yellow-500 text-white hover:scale-105' : 'bg-gray-300 text-gray-700 hover:bg-gray-400'}`} onClick={() => handleTabClick('web_developer')}>Web Development Team</button>
             </div>
 
+
             {/* Team Cards */}
-            <div className="w-full md:w-2/3 lg:w-3/4 mx-auto rounded-lg p-4 md:p-8 lg:p-16">
+            <div className="w-full md:w-2/3 lg:w-3/4 mx-auto rounded-lg p-4 md:p-8 lg:p-12">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8" data-aos="fade-up">
                     {teams[activeTab].map((member, index) => (
                         <TeamCard key={index} member={member} />
