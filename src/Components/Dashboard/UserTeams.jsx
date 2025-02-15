573c2d66dc07306b9875ed0582b271239b8bb66a
arjunmalpani <a4arjunmalpani@gmail.com>
2025-02-15 18:43:46 +0530

    correction in nav

--- src/Components/Dashboard/UserTeams.jsx
+++ src/Components/Dashboard/UserTeams.jsx
@@ -77,7 +77,7 @@ const UserTeams = ({ user }) => {
         </p>
         <Link
           className="bg-black text-white px-4 py-2 rounded-lg hover:bg-teal-600 transition-all duration-300 shadow-md"
-          to="/categories"
+          to="/competitions"
         >
           Participate Now
         </Link>
