8f6cb6f1573fc975b80b56960e9264a788c15a0b
sangam-cm <sangam@cloudmagician.in>
2025-02-27 23:50:32 +0530

    image

--- src/Components/Teams/Teams.jsx
+++ src/Components/Teams/Teams.jsx
@@ -129,7 +129,7 @@ const Teams = () => {
                 <div className="space-y-4 text-center">
                   <img
                     className="w-64 h-64 mx-auto object-cover rounded-xl md:w-40 md:h-40 lg:w-64 lg:h-64"
-                    src="https://i.pinimg.com/736x/a9/28/09/a92809b1860bc5e77faebbb7acebf1a2.jpg"
+                    src="https://www.pgdavhyperion.in/assets/images/hyperion/mentor.jpg"
                     alt="woman"
                     loading="lazy"
                     width="640"
@@ -146,7 +146,7 @@ const Teams = () => {
                 <div className="space-y-4 text-center">
                   <img
                     className="w-64 h-64 mx-auto object-cover rounded-xl md:w-40 md:h-40 lg:w-64 lg:h-64"
-                    src="https://i.pinimg.com/736x/a9/28/09/a92809b1860bc5e77faebbb7acebf1a2.jpg"
+                    src="https://www.pgdavcollege.in/files/userImg/146/User-Image-a5e00132373a7031000fd987a3c9f87b-daacea63cf50d67497d061a43fea777c.JPG"
                     alt="woman"
                     loading="lazy"
                     width="1000"
